17e677fc1e1177753ae5ea7bff0bc355f77fc336
Gabriel Cardoso <gabrielc.cardoso@gmail.com>
2016-07-27 06:06:15 -0300

    Initial commit for Fundraising

---
+++ compile-less.sh
@@ -0,0 +1,7 @@
+cd /Users/gabrielcardoso/Sites/bhaktimarga-website-pages/fundraising-webpage/
+
+cd /Users/bhaktimarga/Documents/Nilkrit\'s\ folder/Google\ Drive/Bhakti\ Marga/Webpages/fundraising-website/
+
+lessc ./less/fundraising.less > ./css/fundraising.css
+
+lessc ./less/bm-overwrite.less > ./css/bm-overwrite.css
