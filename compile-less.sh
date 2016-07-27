82cc6453cc825588dc643b2a24393c2975a9e58a
Gabriel Cardoso <gabrielc.cardoso@gmail.com>
2016-07-27 06:10:32 -0300

    Sanjaya updates for July.

--- compile-less.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/gabrielcardoso/Sites/bhaktimarga-website-pages/fundraising-webpage/
-
-cd /Users/bhaktimarga/Documents/Nilkrit\'s\ folder/Google\ Drive/Bhakti\ Marga/Webpages/fundraising-website/
-
-lessc ./less/fundraising.less > ./css/fundraising.css
-
-lessc ./less/bm-overwrite.less > ./css/bm-overwrite.css
